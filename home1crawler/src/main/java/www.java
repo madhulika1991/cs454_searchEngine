f92bb1f5ebffbe891aad6e85af11fe13cf79e271
pnagabh <pteja125@gmail.com>
2015-03-10 00:24:38 -0500

    updated

--- home1crawler/src/main/java/www.java
+++ home1crawler/src/main/java/www.java
@@ -3,6 +3,12 @@ public class www {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		mpulic static void main
+		public static void main
+		console data()
+		main.string
+		mata fiych dta
+		crawler
 
 	}
 
