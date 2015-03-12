6db874af10b2bac877c4615569d9f4e0f9d70691
pnagabh <pteja125@gmail.com>
2015-03-12 00:33:39 -0500

    updated

--- home1crawler/src/main/java/www.java
+++ home1crawler/src/main/java/www.java
@@ -1,3 +1,8 @@
+import java.io.FileWriter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class www {
 
@@ -7,8 +12,31 @@ public class www {
 		public static void main
 		console data()
 		main.string
+		public static void main(String args[]){
+			
+			String path="c:\\downloaded\\";
+			String name="hw2.json";
+			try {
+			if(args!=null && args.length == 2 && args[0].equals("-c")){
+				FileWriter file = new FileWriter(path+name, true);
+				String fileName=args[1];
+				
+					JSONObject obj = readJsonFile(path + fileName);
+					JSONArray arr=obj.getJSONArray("EmbeddedLinksInfo");
 		mata fiych dta
 		crawler
+		file.write(obj.toString());
+		file.flush();
+		file.close();
+		System.out.println("\n\n\n== MetaData Extraction Done Pls Check "+name);
+		
+	}else{
+		System.out.println("Usage:java -jar extractor.jar -c <control file>");
+	}
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+		
 
 	}
 
